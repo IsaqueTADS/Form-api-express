9781ecd79ceccb0f73a8797cd1982686b46befb4
IsaqueTADS <isaqueofc18@gmail.com>
2025-01-09 19:28:51 -0300

    ajustando status mensagem do front end

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -14,18 +14,23 @@ function submitForm(event) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  }).then((response) => response.json()).then((data) => {
+  })
+    .then((response) => response.json())
+    .then((data) => {
       const statusMessage = document.getElementById("statusMessage");
       statusMessage.style.display = "block";
       statusMessage.style.backgroundColor = "#d4edda";
       statusMessage.style.color = "#155724";
+
       statusMessage.innerHTML = `<strong>Sucesso:</strong> ${data.message}`;
-    //   document.getElementById("emailForm").style.display = "none";
-    }).catch((error) => {
+      //   document.getElementById("emailForm").style.display = "none";
+    })
+    .catch((error) => {
       const statusMessage = document.getElementById("statusMessage");
       statusMessage.style.display = "block";
       statusMessage.style.backgroundColor = "#f8d7da";
       statusMessage.style.color = "#721c24";
+
       statusMessage.innerHTML = `<strong>Erro:</strong> Não foi possível enviar o email. Tente novamente.`;
 
       console.error(error);
