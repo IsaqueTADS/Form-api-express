0c55d1cc4a6fe29f0025d00b42dcebbea1e8feb7
IsaqueTADS <isaqueofc18@gmail.com>
2025-01-09 18:21:33 -0300

    up

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -14,18 +14,14 @@ function submitForm(event) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
+  }).then((response) => response.json()).then((data) => {
       const statusMessage = document.getElementById("statusMessage");
       statusMessage.style.display = "block";
       statusMessage.style.backgroundColor = "#d4edda";
       statusMessage.style.color = "#155724";
       statusMessage.innerHTML = `<strong>Sucesso:</strong> ${data.message}`;
-
-      document.getElementById("emailForm").style.display = "none";
-    })
-    .catch((error) => {
+    //   document.getElementById("emailForm").style.display = "none";
+    }).catch((error) => {
       const statusMessage = document.getElementById("statusMessage");
       statusMessage.style.display = "block";
       statusMessage.style.backgroundColor = "#f8d7da";
