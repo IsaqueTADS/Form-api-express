e884f5ccbe308daf8ea949767f09d8070a5d9bdc
IsaqueTADS <isaqueofc18@gmail.com>
2025-01-09 17:41:45 -0300

    js fetch api front criada

---
+++ public/scripts/script.js
@@ -0,0 +1,39 @@
+const formEmail = document.getElementById("emailForm");
+
+function submitForm(event) {
+  event.preventDefault();
+  const formData = new FormData(formEmail);
+
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+  fetch("/send_email", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const statusMessage = document.getElementById("statusMessage");
+      statusMessage.style.display = "block";
+      statusMessage.style.backgroundColor = "#d4edda";
+      statusMessage.style.color = "#155724";
+      statusMessage.innerHTML = `<strong>Sucesso:</strong> ${data.message}`;
+
+      document.getElementById("emailForm").style.display = "none";
+    })
+    .catch((error) => {
+      const statusMessage = document.getElementById("statusMessage");
+      statusMessage.style.display = "block";
+      statusMessage.style.backgroundColor = "#f8d7da";
+      statusMessage.style.color = "#721c24";
+      statusMessage.innerHTML = `<strong>Erro:</strong> Não foi possível enviar o email. Tente novamente.`;
+
+      console.error(error);
+    });
+}
+
+formEmail.addEventListener("submit", submitForm);
