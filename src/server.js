0479287c9e5e627305bb6fe4f873c146a74289ca
IsaqueTADS <isaqueofc18@gmail.com>
2025-03-03 23:16:30 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 import express from "express";
 import routes from "./api/send_email.js";
 
-
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use("/", routes);
+app.use("/send_email", routes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
