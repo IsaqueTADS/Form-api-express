5c1fed749d724644c62cd260bb1dc208293f066b
IsaqueTADS <isaqueofc18@gmail.com>
2025-03-03 23:20:40 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 import express from "express";
-import routes from "./api/send_email.js";
+import routes from "./api/send_email.js"; // Importa as rotas do arquivo send_email.js
 
 const app = express();
+
+// Middleware para processar dados de formulários e JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Servir arquivos estáticos da pasta "public"
 app.use(express.static("public"));
 
-app.use("/send_email", routes);
+// Usar as rotas definidas no arquivo send_email.js
+app.use("/", routes);
 
+// Definir a porta do servidor
 const PORT = 3000;
+
+// Iniciar o servidor
 app.listen(PORT, () => {
-  console.log(`Servidor ouvindo a porta ${PORT}`);
-  console.log(`http://localhost:3000/`);
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+  console.log(`http://localhost:${PORT}/`);
 });
